4cce9c5afa587b28192365a59a68cd5c10e39a73
Tareq El-Sayyed <tareqelsayyed@gmail.com>
2022-11-09 07:36:46 +0200

    Built the BusinessClassList & EconomyClassList, tested them in the driver, all tests passed.

--- src/FirstClassList.java
+++ src/FirstClassList.java
@@ -64,7 +64,4 @@ public class FirstClassList {
         }
         System.out.println();
     }
-
-
-
 }
--- src/driver.java
+++ src/driver.java
@@ -8,10 +8,23 @@ public class driver {
         airlines.display_airlines();
         FirstClassList fl = new FirstClassList();
         Passenger p1 = new Passenger("Tareq El-Sayyed", "09/05/2003",'M');
-        Passenger p2 = new Passenger("Anthony Badr", "14/05/2002",'M');
+        Passenger p2 = new Passenger("Charbel Abi Khalil", "30/07/2002",'M');
         Passenger p3 = new Passenger("Kevn Abdo", "03/03/2002",'M');
         fl.insert_at_back(p1); fl.insert_at_back(p2); fl.insert_at_back(p3);
         fl.display_first_class_passengers();
 
+        BusinessClassList bl = new BusinessClassList();
+        Passenger p4 = new Passenger("Mohammed Nomaan", "20/05/2001",'M');
+        Passenger p5 = new Passenger("Mohammed Nahhal", "21/09/2001",'M');
+        Passenger p6 = new Passenger("Hadi Youness", "03/03/2002",'M');
+        bl.insert_at_back(p4); bl.insert_at_back(p5); bl.insert_at_back(p6);
+        bl.display_business_class_passengers();
+
+        EconomyClassList el = new EconomyClassList();
+        Passenger p7 = new Passenger("Ali Mansoor", "20/05/2001",'M');
+        Passenger p8 = new Passenger("Anthony Badr", "14/05/2002",'M');
+        Passenger p9 = new Passenger("Anthony Tebshrani", "13/01/2003",'M');
+        el.insert_at_back(p7); el.insert_at_back(p8); el.insert_at_back(p9);
+        el.display_economy_class_passengers();
     }
 }
