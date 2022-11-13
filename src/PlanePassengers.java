af7497313b634d500616c166082d255fb94ee07b
Tareq El-Sayyed <tareqelsayyed@gmail.com>
2022-11-13 11:37:08 +0200

    Built the PlanePassengers class, reworked the input methods in the main and tested the new class out + tweaked bugs in there.

---
+++ src/PlanePassengers.java
@@ -0,0 +1,84 @@
+public class PlanePassengers {
+    public Node first;
+    private String airline;
+
+    // The Constructor Methods.
+    public PlanePassengers(String airline){
+        this.airline = airline;
+        first = null;
+    }
+
+    public String getAirline() {return airline; }
+    public void setAirline(String airline) { this.airline = airline; }
+
+    public boolean is_empty(){ return (first == null); }
+
+    // Mutator Methods
+    // This method inserts an airline at the BEGINNING of the list.
+    public void insert_at_front(Passenger passenger){
+        Node p = new Node(passenger);
+        if(is_empty()){ first = p; }
+        else{
+            p.next = first;
+            p = first;
+        }
+    }
+    // This method inserts an airline at the END of the list.
+    public void insert_at_back(Passenger passenger){
+        Node p = new Node(passenger);
+        if(is_empty()){ first = p; }
+        else{
+            Node current = first;
+            while(current.next != null){ current = current.next; }
+            current.next = p;
+        }
+    }
+    // This method removes an airline from the BEGINNING of the list.
+    public Passenger delete_from_front(){
+        Passenger first_passenger = first.passenger;
+        first = first.next;
+        return first_passenger;
+    }
+    // This method removes an airline from the END of the list.
+    public Passenger delete_from_back(){
+        Passenger last_passenger = null;
+        if (is_empty()){ return null; }
+        else{
+            Node current = first;
+            Node previous = first;
+            while(current.next != null){
+                previous = current;
+                current = current.next;
+            }
+            last_passenger = current.passenger;
+            previous.next = null;
+        }
+        return last_passenger;
+    }
+
+    // The Other Method(s).
+    public String find_passenger(int pos){
+        Node current = first;
+        for (int i = 0; i < 10; i++) {
+            current = current.next;
+        }
+        if(current != null){ return current.passenger.display_passenger(); }
+        return ("Passenger not found!");
+    }
+    public void display_passengers(){
+        System.out.println("---------- PASSENGERS LIST ----------");
+        if (is_empty()){ System.out.println("There are no available passengers on this airline."); }
+        else{
+            System.out.println("All passengers onboard " + getAirline()  + " will be listed below: ");
+            Node current = first;
+            int index = 0;
+            while (current != null){
+                System.out.println((index + 1) + "- " + current.passenger.display_passenger() + " ");
+                current = current.next;
+                index++;
+            }
+        }
+        System.out.println();
+        System.out.println("---------- END PASSENGERS LIST ----------");
+    }
+}
--- src/driver.java
+++ src/driver.java
@@ -7,18 +7,83 @@ public class driver {
         airlines.insert_at_back("MEA"); airlines.insert_at_back("Qatar Airways");
         airlines.insert_at_back("Lufthansa"); airlines.insert_at_back("British Airways");
 
+        PlanePassengers pp1 = new PlanePassengers("Emirates Airlines");
+        PlanePassengers pp2 = new PlanePassengers("Etihad Airways");
+        PlanePassengers pp3 = new PlanePassengers("MEA");
+        PlanePassengers pp4 = new PlanePassengers("Qatar Airways");
+        PlanePassengers pp5 = new PlanePassengers("Lufthansa");
+        PlanePassengers pp6 = new PlanePassengers("British Airways");
+
+
+
+
         airlines.display_airlines();
 
         Passenger p1 = new Passenger("Tareq El-Sayyed", "09/05/2003",'M');
-        System.out.println(p1.getFull_name() + " please enter your destination: ");
-        String destination = input.nextLine();
-        System.out.println(p1.getFull_name() + " please enter an airline: (number)");
-        int airline_number = input.nextInt();
-        System.out.println(p1.getFull_name() + " please enter a class: (number)");
-        int class_number = input.nextInt();
-        String airline = airlines.find_airline(airline_number - 1);
-        Ticket t1 = new Ticket(p1,destination, airline, class_number);
+        LinkedList<Passenger> l = new LinkedList<Passenger>();
+        LinkedList<Ticket> tits = new LinkedList<Ticket>();
 
-        t1.display_ticket();
+        String cont = "";
+        do{
+            System.out.println("Enter your full name: ");
+            String full_name = input.nextLine();
+
+            System.out.println("Enter your date of birth: ");
+            String date_of_birth = input.nextLine();
+
+            System.out.println("Enter your gender: (M/F)");
+            char gender = input.next().charAt(0);
+            input.nextLine();
+
+            Passenger p = new Passenger(full_name, date_of_birth, gender);
+            l.add(p);
+
+            System.out.println(p.getFull_name() + " please enter your destination: ");
+            String destination = input.nextLine();
+
+            System.out.println(p.getFull_name() + " please enter an airline: (number)");
+            int airline_number = input.nextInt();
+
+            System.out.println(p.getFull_name() + " please enter a class: (number)");
+            int class_number = input.nextInt();
+            input.nextLine();
+
+            String airline = airlines.find_airline(airline_number - 1);
+
+            Ticket t = new Ticket(p,destination, airline, class_number);
+            tits.add(t);
+
+            if(airline_number == 1){ pp1.insert_at_back(p); }
+            else if(airline_number == 2){ pp2.insert_at_back(p); }
+            else if(airline_number == 3){ pp3.insert_at_back(p); }
+            else if(airline_number == 4){ pp4.insert_at_back(p); }
+            else if(airline_number == 5){ pp5.insert_at_back(p); }
+            else if(airline_number == 6){ pp6.insert_at_back(p);}
+
+            System.out.println("Do you want to continue? y/n");
+            cont = input.nextLine();;
+
+        }while(cont.equalsIgnoreCase("Y"));
+
+
+        for (int i = 0; i < tits.size(); i++) {
+            Ticket t = tits.get(i);
+            t.display_ticket();
+        }
+
+
+
+        pp1.display_passengers();
+        pp2.display_passengers();
+        pp3.display_passengers();
+        pp4.display_passengers();
+        pp5.display_passengers();
+        pp6.display_passengers();
+
+
+
+
+
+
     }
 }
